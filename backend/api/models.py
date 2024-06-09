8a8694a5411ca295386acc7c3bead32da393efb8
m2zhang <marilyn2zhang@gmail.com>
2024-06-09 17:14:27 -0400

    Update models.py
    Setting up db for Notes

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# For the notes, using ORM (Django automatically converts the Python version of the model's fields and maps it to a corresponding model)
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    # the author is the User. If the user is deleted, CASCADE means we should also delete all the notes
+    # "related_name" is the field name to associate all the notes given a specific User
+
+    def __str__(self):
+        return self.title
