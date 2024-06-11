fde31820bc9b9f99ab21b58c2dd00c10c0833274
m2zhang <marilyn2zhang@gmail.com>
2024-06-11 00:39:04 -0400

    refresh token
    for authorization

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -10,11 +10,41 @@ function ProtectedRoute({children}){
     const [isAuthorized, setIsAuthorized] = useState(null)
 
     const refreshToken = async () => {
-
-    }
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+        try {     
+            const res = await api.post("/api/token/refresh/", {
+                refresh: refreshToken, // to get new access token
+            });
+            if (res.status == 200){ // i.e successful
+                localStorage.setItem(ACCESS_TOKEN, res.data.access)
+                setIsAuthorized(true)
+            } else{
+                setIsAuthorized(false)
+            }
+        }
+        catch(error){
+            console.log(error);
+            setIsAuthorized(false);
+        }
+    };
     
-    const auth = async() => {
+    const auth = async() => { // for automatically refreshing tokens
+        // first see if we have access token, and if it's expired or not (if it is, we'll refresh the token)
+        // else, user needs to login again
+        const token = localStorage.getItem(ACCESS_TOKEN)
+        if (!token){
+            setIsAuthorized(false)
+            return
+        }
+        const decoded = jwtDecode(token) // gives us access to the value
+        const tokenExpiration = decoded.exp
+        const now = Date.now()/1000 // in seconds
 
+        if (tokenExpiration < now){
+            await refreshToken()
+        } else{
+            setIsAuthorized(true)
+        }
     }
 
     if (isAuthorized=== null){
